c605b79fabf57586056c90d731730a715e1f0ffd
nibba228 <witch21277@gmail.com>
2022-02-20 19:18:00 +0300

    Deleted CardSet model considering it is an excessive model layer

--- leitner/cards/models.py
+++ leitner/cards/models.py
@@ -3,17 +3,16 @@ from django.conf import settings
 
 
 class Folder(models.Model):
+    """Папка с карточками"""
+
     title = models.CharField(max_length=29)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
-
-
-class CardSet(models.Model):
-    title = models.CharField(max_length=30)
-    folder = models.ForeignKey(Folder, on_delete=models.CASCADE, related_name='card_set')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 
 class Card(models.Model):
-    card_set = models.ForeignKey(CardSet, related_name='cards', on_delete=models.CASCADE)
+    """Карточка"""
+
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
     content = models.CharField(max_length=100)
     correct_options = models.JSONField()
     options = models.JSONField()
