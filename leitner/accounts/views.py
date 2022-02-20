eb32ea5e3eeab98ef128f810fbee7d6b1af308fa
nibba228 <witch21277@gmail.com>
2022-02-20 14:48:00 +0300

    Added Bootstrap

--- leitner/accounts/views.py
+++ leitner/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def login_view(request):
+    return render(request, 'accounts/login.html')
+
+
+def registration(request):
+    form = UserCreationForm()
+    return render(request, 'accounts/registration.html', context={'form': form})
